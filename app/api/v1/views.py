b4c85b9e2e893775dd2836c84f05506014473a79
lan <lupamo93@gmail.com>
2018-11-28 09:51:48 +0300

    [Feature 162181584] Select individual red-flags

--- app/api/v1/views.py
+++ app/api/v1/views.py
@@ -1,11 +1,11 @@
 from flask import jsonify, make_response, request
-from flask_restful import Resource, Api, reqparse
+from flask_restful import Resource, Api, abort, request
 
 from .models import IncidentsModel
 
 
 class MyIncidents(Resource, IncidentsModel):
-    """ Docstring for my records"""
+    """ Docstring for MyIncidents"""
 
     def __init__(self):
         self.db = IncidentsModel()
@@ -22,11 +22,35 @@ class MyIncidents(Resource, IncidentsModel):
                             comment)
 
         return make_response(jsonify({
-            "Red flag record created": resp
+            "status": 201,
+            "data": [{
+                "id": 1,
+                "message": "Created redflag record"
+            }]
         }), 201)
 
     def get(self):
         resp = self.db.get_incidents()
         return make_response(jsonify({
-            "my list of records": resp
+            "status": 200,
+            "data": resp
         }), 200)
+
+
+class MyRecords(Resource, IncidentsModel):
+    """ Docstring for MyRecords"""
+
+    def __init__(self):
+        self.db = IncidentsModel()
+
+    def get(self, id):
+        incidents = self.db.get_incidents()
+        for i in incidents:
+            if i['id'] == id:
+                print(id)
+                return make_response(jsonify({
+                    "status": 200,
+                    "data": i
+                }), 200)
+
+   
