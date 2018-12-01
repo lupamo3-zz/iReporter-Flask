b2bea8f539c4479a697ac7ff7dc15d5484ef3ad4
lupamo3 <lupamo93@gmail.com>
2018-12-01 12:10:25 +0300

    Changes to Tests

--- app/tests/v1/test_redflag.py
+++ app/tests/v1/test_redflag.py
@@ -42,7 +42,46 @@ class TestRedflags(unittest.TestCase):
         result = json.loads(response.data)
         self.assertEqual(response.status_code, 201)
 
-    
+    def test_get_one_record(self):
+        """ Test if API is able to get a single ID record"""
+        rv = self.client.post(
+            '/api/v1/incidents/1',
+            data=json.dumps(self.data),
+            content_type="application/json"
+        )
+
+        self.assertEqual(rv.status_code, 201)
+        result_in_json = json.loads(rv.data.decode('utf-8').replace("'", "\""))
+        res = self.client.get(
+            '/api/v1/incidents/{}'.format(result_in_json['id']))
+        self.assertEqual(res.status_code, 200)
+
+    def test_patch(self):
+        """Test if the Patch end point is working """
+        rv = self.client.post(
+            '/api/v1/incidents/',
+            data={
+                "createdOn": "2018-11-29 05:21:37",
+                "createdBy": "Norbert",
+                "location": "Mount Sinai",
+                "status": "There is a bush on fire",
+                "comment": "How hot can it be?",
+            }
+        )
+        self.assertEqual(rv.status_code, 201)
+        rv = self.client.put(
+            '/api/v1/incidents/',
+            data={
+                "createdOn": "2019-11-29 05:21:37",
+                "createdBy": "Norberto",
+                "location": "Mount Seenai",
+                "status": "Bush is on fire",
+                "comment": "How hot can i be?",
+            }
+        )
+        self.assertEqual(rv.status_code, 200)
+        results = self.client.get('/api/v1/incidents/1')
+        self.assertIn('How hot can i be?', str(results.data))
 
     def test_records_deletion(self):
         """Test if API can delete existing records """
