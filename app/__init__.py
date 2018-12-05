e8ac5e28cd418cf39f311c3d612ca4b19e4b27f7
lupamo3 <lupamo93@gmail.com>
2018-12-05 16:44:36 +0300

    Return all records after post and patch,

--- app/__init__.py
+++ app/__init__.py
@@ -8,11 +8,11 @@ from .api.v1 import version_1 as v1
 
 # # local  imports
 from instance.config import app_config
-from .database_config import create_tables
+# from .database_config import create_tables
 
 
 def create_app():
     app = Flask(__name__)
-    create_tables()
+    # create_tables()
     app.register_blueprint(v1)
     return app
